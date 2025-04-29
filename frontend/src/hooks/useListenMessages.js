e89e560046c6dceb41ffe57cf187877dd7025e8c
viivekthakur <viivekthakur47@gmail.com>
2025-04-30 01:15:36 +0530

    initial commit'

---
+++ frontend/src/hooks/useListenMessages.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+import { useSocketContext } from "../context/SocketContext";
+import useConversation from "../zustand/useConversation";
+
+import notificationSound from "../assets/sounds/notification.mp3";
+
+const useListenMessages = () => {
+	const { socket } = useSocketContext();
+	const { messages, setMessages } = useConversation();
+
+	useEffect(() => {
+		socket?.on("newMessage", (newMessage) => {
+			newMessage.shouldShake = true;
+			const sound = new Audio(notificationSound);
+			sound.play();
+			setMessages([...messages, newMessage]);
+		});
+
+		return () => socket?.off("newMessage");
+	}, [socket, setMessages, messages]);    //socket because socket changes so we should listen from new socket and messages change means conversation changed and set messages because eslint rules like mention all values on which render should be done
+};
+export default useListenMessages;
